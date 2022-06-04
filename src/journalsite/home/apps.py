41841bb18b67457663c96fd3e8717f22d9871928
Himanshu <himanshu312001@gmail.com>
2022-06-04 07:31:15 +0530

    Added initial files

--- src/journalsite/home/apps.py
+++ src/journalsite/home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JournalsiteConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'journalsite'
+    name = 'home'
